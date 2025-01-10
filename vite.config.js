f1d0cdb9c8c0a02d9320b7799b886737bdc3d51d
maelik <maelik.garo.85@gmail.com>
2025-01-10 17:15:32 +0100

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,39 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate', // Gère automatiquement les mises à jour du Service Worker
+      manifest: {
+        name: 'Application Ker',
+        short_name: 'Ker',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4DBA87',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true, // Active le mode PWA en développement
+        type: 'module' // Utilise le type "module" pour le service worker
+      }
+    })
   ],
   server: {
     host: '0.0.0.0', // Écoute sur toutes les interfaces réseau
